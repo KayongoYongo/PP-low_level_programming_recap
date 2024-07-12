449fb082923b507bd22c48c2f0c474e470b10019
KayongoYongo <sknsammie10@gmail.com>
2024-07-12 05:55:24 +0000

    Print using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main -The function prints out a statement onto the console
+ *
+ *Description: It works FFS
+ *
+ *Return: Always (0) FOr success
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
