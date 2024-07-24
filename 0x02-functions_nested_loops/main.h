a0df31521694e225144a84cd26008e80a62d638a
KayongoYongo <sknsammie10@gmail.com>
2024-07-24 11:08:22 +0300

    Added the function for printing the alphabet 10 times

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,9 @@
 
 /*This program writes chaacters*/
 int _putchar(char c);
+
+/*This program prints the alphabet in lowercase*/
+void print_alphabet(void);
+
+/*Prints 10* the alphabet*/
+void print_alphabet_x10(void);
