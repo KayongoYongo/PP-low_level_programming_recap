8173bf814e0f8b1ff65a9769bf8cdd49c7f76807
KayongoYongo <sknsammie10@gmail.com>
2024-08-12 14:29:01 +0300

    Added main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+/**
+ *_putchar - writes the character c to std
+ *@c: parameter to compare
+ ** Return: 1 or 0
+ **/
+
+/*This program writes chaacters*/
+int _putchar(char c);
+
+/*This program returns 1 if the character is not a digit. Otherwise, returns 0*/
+int _isdigit(int c);
