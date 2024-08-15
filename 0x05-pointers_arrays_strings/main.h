19db3beb5caedc1aa491af25e728687976b390a3
KayongoYongo <sknsammie10@gmail.com>
2024-08-15 16:37:47 +0000

    Updated the functions

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,2 +1,17 @@
 /* 98 battery street */
 void reset_to_98(int *n);
+
+/* Swaps the avlue of two integers */
+void swap_int(int *a, int *b);
+
+/* A function that adds the length of a string */
+int _strlen(char *s);
+
+/* A function that prints  a string followed by a new line */
+void _puts(char *str);
+
+/* A function that prints characters */
+int _putchar(char c);
+
+/* A function that prints a string in reverse */
+void print_rev(char *s);
